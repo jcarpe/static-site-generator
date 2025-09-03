e5a49769505cd73eb87f9291ce7c75400bb2dad8
jcarpe <joe_carpenito@yahoo.com>
2025-09-03 16:02:42 -0400

    add text node delimiter splitting

--- src/conversion.py
+++ src/conversion.py
@@ -17,3 +17,23 @@ def text_node_to_html_node(text_node: TextNode):
             return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
         case _:
             raise Exception("node text type is not a valid TextType")
+        
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if not node.text_type is TextType.TEXT or not delimiter in node.text:
+            # new_nodes.append(node)
+            print("Skipping node:", node)
+        else:
+            parts = node.text.split(delimiter)
+        
+            # Check for invalid markdown (odd number of delimiters)
+            if len(parts) % 2 == 0:
+                raise ValueError(f"Invalid markdown syntax: unmatched {delimiter}")
+            
+            for i, part in enumerate(parts):
+                if part:  # Skip empty parts
+                    # Even indices are normal text, odd indices are formatted text
+                    current_type = text_type if i % 2 == 1 else TextType.TEXT
+                    new_nodes.append(TextNode(part, current_type))
+    return new_nodes
--- src/test_conversion.py
+++ src/test_conversion.py
@@ -1,7 +1,7 @@
 import unittest
 from textnode import TextNode, TextType
 from htmlnode import LeafNode
-from conversion import text_node_to_html_node
+from conversion import text_node_to_html_node, split_nodes_delimiter
 
 class TestHTMLNodeConversion(unittest.TestCase):
     def test_text_node_to_html_node(self):
@@ -40,6 +40,83 @@ class TestHTMLNodeConversion(unittest.TestCase):
         text_node_wrong = TextNode("Hello, world!", "wrong")
         with self.assertRaises(Exception):
             text_node_to_html_node(text_node_wrong)
+
+class TestNodeSplitting(unittest.TestCase):
+    def test_split_nodes_bold_delimiter(self):
+        old_nodes = [
+            TextNode("Hello, **world**!", TextType.TEXT)
+        ]
+        delimiter = "**"
+        text_type = TextType.BOLD
+
+        new_nodes = split_nodes_delimiter(old_nodes, delimiter, text_type)
+
+        self.assertEqual(len(new_nodes), 3)
+
+        self.assertEqual(new_nodes[0].text, "Hello, ")
+        self.assertEqual(new_nodes[1].text, "world")
+        self.assertEqual(new_nodes[2].text, "!")
+
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text_type, TextType.BOLD)
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+
+    def test_split_nodes_italics_delimiter(self):
+        old_nodes = [
+            TextNode("Hello, _world_!", TextType.TEXT)
+        ]
+        delimiter = "_"
+        text_type = TextType.ITALIC
+
+        new_nodes = split_nodes_delimiter(old_nodes, delimiter, text_type)
+
+        self.assertEqual(len(new_nodes), 3)
+
+        self.assertEqual(new_nodes[0].text, "Hello, ")
+        self.assertEqual(new_nodes[1].text, "world")
+        self.assertEqual(new_nodes[2].text, "!")
+
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text_type, TextType.ITALIC)
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+
+    def test_split_nodes_code_delimiter(self):
+        old_nodes = [
+            TextNode("Hello, `world`!", TextType.TEXT)
+        ]
+        delimiter = "`"
+        text_type = TextType.CODE
+
+        new_nodes = split_nodes_delimiter(old_nodes, delimiter, text_type)
+
+        self.assertEqual(len(new_nodes), 3)
+
+        self.assertEqual(new_nodes[0].text, "Hello, ")
+        self.assertEqual(new_nodes[1].text, "world")
+        self.assertEqual(new_nodes[2].text, "!")
+
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text_type, TextType.CODE)
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+
+    def test_split_nodes_delimiter_multiple_instances(self):
+        old_nodes = [
+            TextNode("**Hello**, **world**!", TextType.TEXT)
+        ]
+        delimiter = "**"
+        text_type = TextType.BOLD
+
+        new_nodes = split_nodes_delimiter(old_nodes, delimiter, text_type)
+
+        self.assertEqual(new_nodes[0].text, "Hello")
+        self.assertEqual(new_nodes[1].text, ", ")
+        self.assertEqual(new_nodes[2].text, "world")
+        self.assertEqual(new_nodes[3].text, "!")
+
+        self.assertEqual(new_nodes[0].text_type, TextType.BOLD)
+        self.assertEqual(new_nodes[1].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[2].text_type, TextType.BOLD)
+        self.assertEqual(new_nodes[3].text_type, TextType.TEXT)
 
 if __name__ == "__main__":
     unittest.main()
