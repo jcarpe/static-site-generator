abb5a6483bdcd3f1e28241cd40fcfdbe23e410a9
jcarpe <joe_carpenito@yahoo.com>
2025-09-03 12:08:22 -0400

    initial tests

---
+++ src/main.py
@@ -0,0 +1,5 @@
+from textnode import TextNode, TextType
+
+node = TextNode("Hello, World!", TextType.CODE, "http://example.com")
+
+print(node)
---
+++ src/test_textnode.py
@@ -0,0 +1,22 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_not_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a different text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_included_url(self):
+        node = TextNode("This is a text node", TextType.BOLD, "http://example.com")
+        self.assertIn("http://example.com", node.url)
+
+if __name__ == "__main__":
+    unittest.main()
